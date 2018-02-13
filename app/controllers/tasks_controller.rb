790527e1cfe11619a8c1b582dd955cc7aa6da5c9
AlinaKhalilova <akhalilova@alumni.escaet.fr>
2018-02-13 14:58:51 +0100

    exo1 : Controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+   def create
+    @task = Task.new(
+      title: params[:title],
+      details: params[:details])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+  @task = Task.find(params[:id])
+  @task.update[params[:task]]
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+end
+
+
+
+
